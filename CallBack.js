8446a5c57b7c4f3ac0e48f75b0c15db743696012
nandanchakraborty <nandancsebubt@gmail.com>
2026-01-29 19:23:00 +0600

    added a module called Course

--- CallBack.js
+++ CallBack.js
@@ -1,9 +1,7 @@
 function display(value){
     document.getElementById("demo").innerHTML = value
 
-
 }
-
 function calculation (value1 ,value2 ,callback){
     let sum = value1+value2;
 if(callback){
@@ -12,7 +10,60 @@ if(callback){
 return sum;
 
 }
-
 const result = calculation(5,5);
 calculation(4,5,display);
 console.log(result);
+
+
+
+
+
+
+
+
+
+
+const paymentSuccess = true;
+let marks = 70;
+function enroll(callback){
+    console.log('payment in progress');
+
+   setTimeout(function(){
+    if(paymentSuccess){
+        callback();
+    }
+    else{
+        console.log('payment failed');
+
+    }
+},2000)
+}  
+
+
+function progress(callback){
+    console.log('course in progress');
+
+    setTimeout(function(){
+    
+        if(marks >=80){
+            callback();
+        }
+        else {
+            console.log('not enough mark');
+
+
+ } },3000);
+}
+
+function getCertificate(){
+console.log('preparing certificate');
+setTimeout(function(){
+    console.log('u got it');
+
+},1000)
+}
+
+enroll(function(){
+    progress(getCertificate);
+})
+
